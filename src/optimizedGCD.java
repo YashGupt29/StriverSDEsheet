5966f95fc0d200c9ba115b459818f7afb766e4f8
Yash Gupta <yashnitagartala8@gmail.com>
2023-10-17 06:48:20 +0530

    GCD

--- src/optimizedGCD.java
+++ src/optimizedGCD.java
@@ -1,2 +1,21 @@
 public class optimizedGCD {
+    public static int calcGCD(int n, int m){
+        // Write your code here.
+        if(n==m)
+        {
+            return m;
+        }
+        int max=Math.max(n,m);
+        int min=Math.min(n,m);
+        int GCD=0;
+
+        for(int i=1;i<=(max-min);i++)
+        {
+            if(n%i==0 && m%i==0)
+            {
+                GCD=i;
+            }
+        }
+        return GCD;
+    }
 }
