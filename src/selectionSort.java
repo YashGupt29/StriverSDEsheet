85571f1167d4f1c0fa554a425a227c733666bbfd
Yash Gupta <yashnitagartala8@gmail.com>
2023-10-27 06:37:49 +0530

    insertion sort

--- src/selectionSort.java
+++ src/selectionSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class selectionSort {
     public static void main(String[] args) {
         int [] arr={12,46,24,52,20,9};
-        System.out.println(Arrays.toString(sortByBubble(arr)));
+        System.out.println(Arrays.toString(sortbyInsertion(arr)));
 
     }
     static  int [] sortBySelection(int [] arr)
@@ -42,6 +42,24 @@ public class selectionSort {
                 }
 
             }
+        }
+        return arr;
+    }
+    static  int [] sortbyInsertion(int [] arr)
+    {
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j])
+            {
+                int temp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=temp;
+                j--;
+
+            }
+
         }
         return arr;
     }
