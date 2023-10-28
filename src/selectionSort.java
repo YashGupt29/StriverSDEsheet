0bdffaa4356a19be70d59aa8f35f05866f052e20
Yash Gupta <yashnitagartala8@gmail.com>
2023-10-28 11:39:56 +0530

    merge sort

--- src/selectionSort.java
+++ src/selectionSort.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class selectionSort {
     public static void main(String[] args) {
         int [] arr={12,46,24,52,20,9};
-        System.out.println(Arrays.toString(sortbyInsertion(arr)));
-
+        System.out.println(Arrays.toString(sortByMerging(arr)));
     }
     static  int [] sortBySelection(int [] arr)
     {
@@ -29,6 +29,7 @@ public class selectionSort {
     }
     static int []  sortByBubble(int [] arr)
     {
+        boolean swapped=false;
         int n=arr.length;
         for(int i=n-1;i>=0;i--)
         {
@@ -39,8 +40,13 @@ public class selectionSort {
                     int temp=arr[j+1];
                     arr[j+1]=arr[j];
                     arr[j]=temp;
+                    swapped=true;
                 }
 
+            }
+            if(!swapped)
+            {
+                break;
             }
         }
         return arr;
@@ -63,5 +69,58 @@ public class selectionSort {
         }
         return arr;
     }
+    static int [] sortByMerging(int [] arr){
+        mergeSort(arr,0,arr.length-1);
+        return arr;
+    }
+
+    static  void mergeSort(int [] arr, int low, int high)
+    {
+        if(low>=high)
+        {
+            return;
+        }
+        int mid=(low+high)/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+
+
+
+    }
+    static void merge(int [] arr,int low,int mid,int high)
+    {
+        ArrayList <Integer> temp=new ArrayList<>();
+        int left=low;
+        int right=mid+1;
+        while(left<=mid && right<=high)
+        {
+            if(arr[left]<=arr[right])
+            {
+                temp.add(arr[left] );
+                left++;
+            }
+            else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while(right<=high)
+        {
+            temp.add(arr[right]);
+            right++;
+
+        }
+        while(left<=mid)
+        {
+            temp.add(arr[left]);
+            left++;
+        }
+        for(int i=low;i<=high;i++)
+        {
+            arr[i]= temp.get(i - low);
+        }
+    }
+
 
 }
