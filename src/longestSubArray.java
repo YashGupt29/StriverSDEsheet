e54495451a0e426e612874930a96e38dc10313a8
YashGupt29 <yashnitagartala8@gmail.com>
2023-11-07 07:11:57 +0530

    optimal solution

--- src/longestSubArray.java
+++ src/longestSubArray.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class longestSubArray {
 
     public static void main(String[] args) {
-        int [] arr={1,2,3,1,1,1,1};
+        int [] arr={2,0,0,3};
 
-        System.out.println( longestSubarrayWithSumK(arr,3));
+        System.out.println( longestSubarrayWithSumKOptimal(arr,3));
 
     }
     public static int longestSubarrayWithSumK(int []a, long k)
@@ -29,6 +31,34 @@ public class longestSubArray {
                 }
             }
 
+
+        }
+        return maxLength;
+
+    }
+    public static int longestSubarrayWithSumKOptimal(int []a, long k)
+    {
+        HashMap<Long,Integer> preSum=new HashMap<>();
+        long sum=0;
+        int maxLength=0;
+        for (int i = 0; i < a.length; i++)
+        {
+            sum+=a[i];
+            if(sum==k)
+            {
+                maxLength=Math.max(maxLength,i+1);
+            }
+
+            long rem=sum-k;
+            if(preSum.containsKey(rem))
+            {
+              int length=i-preSum.get(rem);
+              maxLength=Math.max(length,maxLength);
+            }
+            if(!preSum.containsKey(sum))
+            {
+                preSum.put(sum,i);
+            }
 
         }
         return maxLength;
