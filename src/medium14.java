a920982dad7b8fc42823b0e0a9cacb3e8b1cd491
YashGupt29 <yashnitagartala8@gmail.com>
2024-03-27 16:20:15 +0530

    medium14

---
+++ src/medium14.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class medium14 {
+    public static void main(String[] args) {
+
+
+    }
+    public int subarraySum(int[] arr, int k)
+    {
+        int n = arr.length;
+        Map <Integer,Integer >mpp = new HashMap();
+        int preSum = 0, cnt = 0;
+
+        mpp.put(0, 1);
+        for (int i = 0; i < n; i++) {
+            preSum += arr[i];
+            int remove = preSum - k;
+            cnt += mpp.getOrDefault(remove, 0);
+            mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
+        }
+        return cnt;
+    }
+}
