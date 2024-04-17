a861f9ca97bb97e1752b7e245f84f60ec8089020
YashGupt29 <yashnitagartala8@gmail.com>
2024-04-17 16:19:50 +0530

    done bs8

--- src/bs8.java
+++ src/bs8.java
@@ -2,12 +2,11 @@ import java.util.Arrays;
 
 public class bs8 {
     public static void main(String[] args) {
-        int[] arr={1,3};
-        System.out.println(index(arr));
+        int[] arr={7,8,9,1,2,3,4,5,6};
         System.out.println(search(arr,1));
 
     }
-    public static int search(int[] nums, int target) {
+    public static int searchBrute(int[] nums, int target) {
         int index=index(nums);
         Arrays.sort(nums);
         return binarysearch(nums,target,index);
@@ -52,6 +51,41 @@ public class bs8 {
         }
         return index;
 
+    }
+    public static int search(int[] nums, int target)
+    {
+        int first=0;
+        int last=nums.length-1;
+        while(first<=last)
+        {
+           int mid=first+(last-first)/2;
+           if(nums[mid]==target)
+           {
+               return mid;
+           }
+           if(nums[first]<=nums[mid])
+           {
+
+               if(nums[first]<=target && target<=nums[mid])
+               {
+                   last=mid-1;
+               }
+               else {
+                   first=mid+1;
+               }
+           }
+           else {
+               if((target>=nums[mid] && target<=nums[last]))
+               {
+                   first=mid+1;
+               }
+               else {
+                   last=mid-1;
+               }
+
+           }
+        }
+        return -1;
     }
 
 }
