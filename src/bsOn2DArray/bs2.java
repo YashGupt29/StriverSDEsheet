ce477bce5a825a1f91c6935c68d6730b5a5e0afc
YashGupt29 <yashnitagartala8@gmail.com>
2024-05-17 13:28:58 +0530

    done

--- src/bsOn2DArray/bs2.java
+++ src/bsOn2DArray/bs2.java
@@ -3,46 +3,34 @@ package bsOn2DArray;
 public class bs2 {
     public static void main(String[] args) {
         int[][] matrix = {
-                {1, 3, 5, 7},
-                {10, 11, 16, 20},
-                {23, 30, 34, 60}
+                {1,1}
         };
-        int target = 20;
+        int target = 1;
         System.out.println(searchMatrix(matrix,target));
 
     }
-    public static boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
-            if(findTarget(matrix[i],target))
+    public static boolean searchMatrix(int[][] matrix, int target)
+    {
+        int n= matrix[0].length;
+        int row=0;
+        int col=n-1;
+        while(row< matrix.length && col>=0)
+        {
+            if(matrix[row][col]==target)
             {
                 return true;
-
+            }
+            if(matrix[row][col]<target)
+            {
+                row++;
+            }
+            else
+            {
+                col--;
             }
 
         }
         return false;
-
     }
-    public static boolean findTarget(int [] arr,int target)
-    {
-       int first=0;
-       int last=arr.length-1;
-       while(first<=last)
-       {
-           int mid=first+(last-first)/2;
-           if(arr[mid]==target)
-           {
-               return true;
-           }
-           if(target>arr[mid])
-           {
-               first=mid+1;
-           }
-           else {
-               last=mid-1;
-           }
-       }
-       return false;
 
-    }
 }
