e0bd9a08938008f6abda23e6b8dc741d04711994
YashGupt29 <yashnitagartala8@gmail.com>
2024-06-03 13:55:47 +0530

    optimized

--- src/LinkedList/medium/l4.java
+++ src/LinkedList/medium/l4.java
@@ -22,5 +22,21 @@ public class l4 {
 
 
     }
+    public boolean hasCycleOptimized(ListNode head) {
+        if(head==null)
+        {
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+
+        }
+        return false;
+    }
 
 }
