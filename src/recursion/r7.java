335dad21424b4968d591f0125cc17fa6c0aeeda8
YashGupt29 <yashnitagartala8@gmail.com>
2024-06-08 18:51:15 +0530

    done r7

---
+++ src/recursion/r7.java
@@ -0,0 +1,34 @@
+package recursion;
+
+public class r7 {
+    public static void main(String[] args)
+    {
+        int [] arr={2,5,1,4,3};
+        System.out.println(perfectSum(arr,0,10));
+
+    }
+
+
+    public static int perfectSum(int arr[],int n, int sum)
+    {
+        return perfectSumHelper(arr,sum,0,0,0);
+
+    }
+    public static int perfectSumHelper(int arr[],int sum,int count,int index,int ans )
+    {
+        if(index>=arr.length)
+        {
+            if(ans==sum)
+            {
+                count++;
+            }
+            return count;
+        }
+        if(ans<=sum) {
+            count = perfectSumHelper(arr, sum, count, index + 1, ans + arr[index]);
+            count = perfectSumHelper(arr, sum, count, index + 1, ans);
+        }
+        return count;
+    }
+
+}
